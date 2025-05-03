db3677ce2a074c5a50556836ab21adf14b268ac7
Robert Cowher <bobcowher@gmail.com>
2025-05-03 10:42:35 -0500

    initial mostly working init.lua

---
+++ init.lua
@@ -0,0 +1,71 @@
+vim.wo.relativenumber = true
+
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
+vim.opt.clipboard = "unnamedplus"
+
+-- Load Lazy
+vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
+
+require("lazy").setup({
+
+  -- Language Server Protocol
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("lspconfig").clangd.setup {}
+    end
+  },
+
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip"
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }),
+      })
+    end
+  },
+
+  -- Treesitter (syntax highlighting)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "python", "bash", "json", "c", "cpp" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+
+  -- File explorer
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require("nvim-tree").setup({})
+    end
+  },
+})
+
